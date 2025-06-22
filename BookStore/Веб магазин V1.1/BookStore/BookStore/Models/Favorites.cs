064913aae54251b9b9df47429687b834364666cd
Evgenij1234 <116650874+Evgenij1234@users.noreply.github.com>
2025-06-23 03:05:57 +0700

    ++

--- BookStore/Веб магазин V1.1/BookStore/BookStore/Models/Favorites.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookStore
-{
-    public class Favorites
-    {
-        public int ID { get; set; }
-        public int? idBooks { get; set; }
-        public int? quantity { get; set; }
-    }
-}
--- BookStore/Веб магазин V1.1/BookStore/BookStore/Models/Place.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookStore.Models
-{
-    public class Place
-    {
-        public int ID { get; set; }
-        public string? place { get; set; }
-    }
-}
